02438891c059a63e9d79b6a0303e5cdb20ffcf4e
Joshua Berger <bergerjosh11@gmail.com>
2023-09-03 14:10:22 -0600

    Update train_model.py

--- models/train_model.py
+++ models/train_model.py
@@ -1,121 +1,48 @@
-import argparse
-import pandas as pd
-import numpy as np
-import alpaca_trade_api as tradeapi
+import os
 import joblib
-from sklearn.ensemble import RandomForestClassifier  # Example model, replace with your model
+import alpaca_trade_api as tradeapi  # Replace with your data source API library
 
-# Add command-line argument support
-parser = argparse.ArgumentParser(description='Moving Average Crossover Trading Strategy')
-parser.add_argument('--symbol', type=str, required=True, help='Stock symbol (e.g., AAPL)')
-parser.add_argument('--data-file', type=str, required=True, help='Path to historical data CSV file')
-parser.add_argument('--api-key', type=str, required=True, help='Your Alpaca API Key')
-parser.add_argument('--api-secret', type=str, required=True, help='Your Alpaca API Secret')
-args = parser.parse_args()
+class TrainedModels:
+    def __init__(self, model_folder='models/saved_models'):
+        self.model_folder = model_folder
+        self.models = {}  # A dictionary to store loaded models
+        self.load_models()
 
-# Initialize Alpaca API with your credentials
-api = tradeapi.REST(args.api_key, args.api_secret, base_url='https://paper-api.alpaca.markets')  # Use paper trading URL for testing
+    def load_models(self):
+        # Clear any existing models
+        self.models = {}
 
-# Load historical stock data from the specified CSV file
-try:
-    data = pd.read_csv(args.data_file)
-except FileNotFoundError:
-    print(f"Error: The specified data file '{args.data_file}' was not found.")
-    exit(1)
+        # Load models from the model folder
+        for filename in os.listdir(self.model_folder):
+            if filename.endswith("_model.pkl"):
+                symbol = filename.split("_")[0]  # Extract the stock symbol
+                model_path = os.path.join(self.model_folder, filename)
+                model = joblib.load(model_path)
+                self.models[symbol] = model
 
-# Define moving average window periods
-short_window = 50
-long_window = 200
-
-# Function to check moving average crossover
-def moving_average_cross(data):
-    short_ma = data['Close'].rolling(window=short_window).mean()
-    long_ma = data['Close'].rolling(window=long_window).mean()
-    if short_ma.iloc[-1] > long_ma.iloc[-1] and short_ma.iloc[-2] <= long_ma.iloc[-2]:
-        return 'buy'
-    elif short_ma.iloc[-1] < long_ma.iloc[-1] and short_ma.iloc[-2] >= long_ma.iloc[-2]:
-        return 'sell'
-    else:
-        return 'hold'
-
-# Function to execute trades based on signals and account balance
-def execute_trades(api, symbol, action, quantity, balance):
-    last_price = api.get_latest_trade(symbol).price
-    if action == 'buy':
-        if balance >= last_price * quantity:
-            api.submit_order(
-                symbol=symbol,
-                qty=quantity,
-                side='buy',
-                type='limit',
-                limit_price=last_price,
-                time_in_force='gtc'
-            )
-            print(f'Bought {quantity} shares of {symbol} at {last_price} per share.')
-            balance -= last_price * quantity
+    def get_model(self, symbol):
+        if symbol in self.models:
+            return self.models[symbol]
         else:
-            print(f'Insufficient funds to buy {quantity} shares of {symbol}.')
-    elif action == 'sell':
-        positions = api.list_positions()
-        for position in positions:
-            if position.symbol == symbol and position.qty >= quantity:
-                api.submit_order(
-                    symbol=symbol,
-                    qty=quantity,
-                    side='sell',
-                    type='limit',
-                    limit_price=last_price,
-                    time_in_force='gtc'
-                )
-                print(f'Sold {quantity} shares of {symbol} at {last_price} per share.')
-                balance += last_price * quantity
-                break
+            print(f"Model for {symbol} not found.")
+            return None
+
+if __name__ == "__main__":
+    # Initialize the data source API (e.g., Alpaca)
+    api = tradeapi.REST('YOUR_API_KEY', 'YOUR_API_SECRET', base_url='https://paper-api.alpaca.markets')
+
+    # Fetch a list of stock symbols dynamically from your data source
+    stock_symbols = [asset.symbol for asset in api.list_assets() if asset.tradable]
+
+    # Initialize the TrainedModels class to load saved models
+    trained_models = TrainedModels()
+
+    for symbol_to_check in stock_symbols:
+        # Retrieve the trained model for each symbol
+        model_for_symbol = trained_models.get_model(symbol_to_check)
+
+        if model_for_symbol:
+            # You can use the loaded model for predictions or other tasks
+            print(f"Loaded model for {symbol_to_check}.")
         else:
-            print(f'No {symbol} position to sell or insufficient shares.')
-
-# Train a simple machine learning model (Example: Random Forest Classifier, replace with your model)
-def train_machine_learning_model(data):
-    features = data[['Close', 'Volume']]  # Use relevant features from your data
-    labels = data['Signal']  # Assuming you have a 'Signal' column with 'buy', 'sell', or 'hold' labels
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(features, labels)
-    return model
-
-# Train your machine learning model
-model = train_machine_learning_model(data)
-
-# Save the trained model to the 'saved_models' folder
-model_filename = f'saved_models/{args.symbol}_model.pkl'
-joblib.dump(model, model_filename)
-print(f'Saved the trained model to {model_filename}')
-
-# Initialize a starting balance for simulation (or connect to your real account)
-balance = 100000  # Starting balance in dollars
-shares_held = 0   # Number of shares currently held
-
-# Main trading loop
-while True:
-    try:
-        # Fetch historical stock data
-        stock_data = data[data['Symbol'] == args.symbol].copy()
-        action = moving_average_cross(stock_data)
-
-        # Get account balance (or use your real account balance)
-        account = api.get_account()
-        balance = float(account.cash)
-
-        if action == 'buy':
-            # Define the quantity to buy based on your balance
-            # For example, you can allocate 50% of your balance to this trade
-            max_buy_quantity = int(balance * 0.5 / stock_data['Close'].iloc[-1])
-            if max_buy_quantity > 0:
-                execute_trades(api, args.symbol, 'buy', max_buy_quantity, balance)
-        elif action == 'sell':
-            # Define the quantity to sell based on your current position size
-            # For example, you can sell half of your current position
-            max_sell_quantity = int(balance * 0.5 / stock_data['Close'].iloc[-1])
-            if max_sell_quantity > 0:
-                execute_trades(api, args.symbol, 'sell', max_sell_quantity, balance)
-
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
+            print(f"Model for {symbol_to_check} not found.")
