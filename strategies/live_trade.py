2b92197ff8e0b737a25a439a395bd7cf578f0201
Joshua Berger <bergerjosh11@gmail.com>
2023-09-03 14:19:06 -0600

    Update live_trade.py

--- strategies/live_trade.py
+++ strategies/live_trade.py
@@ -1,70 +1,58 @@
-# utils.py
 import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier  # Replace with your model
+import alpaca_trade_api as tradeapi
+from models.trained_models import TrainedModels
+from strategies.utils import execute_trades
 
-# Function to check moving average crossover
-def moving_average_cross(data):
-    short_ma = data['Close'].rolling(window=50).mean()
-    long_ma = data['Close'].rolling(window=200).mean()
-    if short_ma.iloc[-1] > long_ma.iloc[-1] and short_ma.iloc[-2] <= long_ma.iloc[-2]:
-        return 'buy'
-    elif short_ma.iloc[-1] < long_ma.iloc[-1] and short_ma.iloc[-2] >= long_ma.iloc[-2]:
-        return 'sell'
-    else:
-        return 'hold'
+# Initialize Alpaca API with your credentials
+api = tradeapi.REST('YOUR_API_KEY', 'YOUR_API_SECRET', base_url='https://paper-api.alpaca.markets')
 
-# Function to execute trades based on signals and account balance
-def execute_trades(api, symbol, action, quantity):
-    last_price = api.get_latest_trade(symbol).price
-    if action == 'buy':
-        api.submit_order(
-            symbol=symbol,
-            qty=quantity,
-            side='buy',
-            type='limit',
-            limit_price=last_price,
-            time_in_force='gtc'
-        )
-    elif action == 'sell':
-        api.submit_order(
-            symbol=symbol,
-            qty=quantity,
-            side='sell',
-            type='limit',
-            limit_price=last_price,
-            time_in_force='gtc'
-        )
-
-# Your AI model class should implement the get_signal method
-import joblib
-import os
-from glob import glob
-
-# Your AI model class should implement the get_signal method
 class YourTradingModel:
     def __init__(self, model_folder='models/saved_models'):
         self.model_folder = model_folder
         self.load_most_recent_model()
+        self.trained_models = TrainedModels(self.model_folder)
 
     def load_most_recent_model(self):
-        model_files = glob(os.path.join(self.model_folder, '*.pkl'))
-        if model_files:
-            most_recent_model_file = max(model_files, key=os.path.getctime)
-            self.model = joblib.load(most_recent_model_file)
-        else:
-            raise FileNotFoundError(f"No model files found in '{self.model_folder}'.")
+        self.trained_models = TrainedModels(self.model_folder)
 
-    def get_signal(self, data):
-        # Prepare the data for prediction
-        features = data[['Close', 'Volume']].values.reshape(1, -1)  # Example features, replace with your features
-        prediction = self.model.predict(features)
+    def get_signal(self, symbol):
+        stock_data = api.get_barset(symbol, 'day', limit=200).df[symbol]
+        signal = self.trained_models.get_model(symbol).predict(stock_data)
+        return signal
 
-        # Map the prediction to a trading action
-        if prediction == 'buy':
-            return 'buy'
-        elif prediction == 'sell':
-            return 'sell'
-        else:
-            return 'hold'
+def determine_symbol_to_trade():
+    assets = api.list_assets()
+    for asset in assets:
+        if asset.tradable:
+            model = YourTradingModel()
+            signal = model.get_signal(asset.symbol)
+            if signal == 'buy':
+                return asset.symbol
+    return None
 
+def calculate_trade_quantity(account_balance, last_price):
+    # You can customize this logic based on your risk tolerance and strategy
+    max_allocation = account_balance * 0.1  # Allocate 10% of the account balance
+    max_quantity = max_allocation / last_price
+    return int(max_quantity)
+
+if __name__ == "__main__":
+    while True:
+        try:
+            symbol_to_trade = determine_symbol_to_trade()
+            if symbol_to_trade:
+                stock_data = api.get_barset(symbol_to_trade, 'day', limit=1).df[symbol_to_trade]
+                last_price = stock_data['close'].iloc[0]
+                account = api.get_account()
+                account_balance = float(account.cash)
+                quantity_to_trade = calculate_trade_quantity(account_balance, last_price)
+
+                model = YourTradingModel()
+                signal = model.get_signal(symbol_to_trade)
+
+                if signal == 'buy':
+                    execute_trades(api, symbol_to_trade, 'buy', quantity_to_trade)
+                elif signal == 'sell':
+                    execute_trades(api, symbol_to_trade, 'sell', quantity_to_trade)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
